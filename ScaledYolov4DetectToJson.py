a7960718945030470c3eeede876589abfe88d861
Huang82 <90618109@gcloud.csu.edu.tw>
2021-06-24 14:22:12 +0800

    update isGlaucoma

--- ScaledYolov4DetectToJson.py
+++ ScaledYolov4DetectToJson.py
@@ -20,6 +20,8 @@ from utils.general import (
     check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, plot_one_box, strip_optimizer)
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
+# from isGlaucoma import IsGlaucoma
+
 # python ScaledYolov4DetectToJson.py --weights ./runs/exp0_yolov4-csp-results/weights/best_yolov4-csp-results.pt --img 416 --conf 0.4 --source ./test/images 
 
 # add Data json
@@ -90,60 +92,6 @@ def getShapes(shapes, labelName, positionXY, group_id, shape_type, flags):
 def createDirImage(image_dirPath):
     os.makedirs(image_dirPath)
 
-# # is glaucoma
-# def IsGlaucoma(shapes):
-
-#     w1 = 0
-#     h1 = 0
-    
-#     w2 = 0
-#     h2 = 0
-
-#     for data in shapes:
-#         print(data)
-#         # small (1)
-#         if data['label'] == "cup":
-#             bx1 = data['points'][0][0]
-#             bx2 = data['points'][1][0]
-#             by1 = data['points'][0][1]
-#             by2 = data['points'][1][1]
-#             w1 = abs(bx2 - bx1)
-#             h1 = abs(by2 - by1)
-
-#         # big   (2)
-#         if data['label'] == "disk":
-#             ax1 = data['points'][0][0]
-#             ax2 = data['points'][1][0]
-#             ay1 = data['points'][0][1]
-#             ay2 = data['points'][1][1]
-#             w2 = abs(ax2 - ax1)
-#             h2 = abs(ay2 - ay1)
-#     print("\nw1: {0}, w2: {1}, h1:{2}, h2:{3}".format(w1, w2, h1, h2))
-#     if w1 < w2 and h1 < h2:
-#         ratio_w = w1 / w2
-#         ratio_h = h1 / h2
-#         print("")
-#         # print("w1: {0}, h1: {1}, w2: {2}, h2: {3}".format(w1, h1, w2, h2))
-#         # print("")
-#         # print("ratio_w: {0}, ratio_h: {1}".format(ratio_w, ratio_h))
-#         # print("")
-#         if ratio_w > 0.7 and ratio_h > 0.7:
-#           S = by1 - ay1
-#           I = ay2 - by2
-#           print("\nS: {0}, I: {1}".format(S, I))
-#           if I < 0 or S < 0:
-#             # 框預測超過大的框會跑來這
-#             print("I or S is minus.")
-#           elif I > S:
-#             print("I > S, No glaucoma.")
-#           else:
-#             print("Yes glaucoma.")
-#         else:
-#             print("ratio < 0.7, No glaucoma")
-
-#     else:
-#         # 可能長或寬超過比較大的框才跑來這
-#         print("Width or height Error!")
     
 
 def detect(save_img=False):
@@ -253,12 +201,8 @@ def detect(save_img=False):
                         # create shapes format
                         getShapes(shapes, names[int(cls)], torch.tensor(xyxy).view(1, 4).view(-1).tolist(), None, "rectangle", {})
 
-            # # cal glaucoma
-            # if boxes == 2:
-            #     IsGlaucoma(shapes)
-            # else:
-                # print("\nbox != 2, box:{0}".format(boxes))
-
+            # cal glaucoma
+            # IsGlaucoma(shapes)
 
             # Print time (inference + NMS)
             print('%sDone. (%.3fs)' % (s, t2 - t1))

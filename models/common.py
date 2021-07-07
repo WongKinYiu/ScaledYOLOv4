8cb4123fd4d21cbe788ae73604eb56ac77d73dc5
Ahmed Mesih <74631008+AhmedMesih@users.noreply.github.com>
2021-07-07 12:12:42 +0300

    update

--- models/common.py
+++ models/common.py
@@ -476,7 +476,7 @@ class ConvSqu(nn.Module):
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
         super(ConvSqu, self).__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
-        self.act = nn.Mish() if act else nn.Identity()
+        self.act = Mish() if act else nn.Identity()
 
     def forward(self, x):
         return self.act(self.conv(x))
